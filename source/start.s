611e0e1ed2afe9ad7e82fb358885a578abf94359
archshift <admin@archshift.com>
2015-02-01 14:09:56 -0800

    Fix gray screen issue #3
    Thanks, @Normmatt!

--- source/start.s
+++ source/start.s
@@ -1,31 +1,49 @@
-.section .data
-.balign 4
-run_once: .word 1
-
-.section .text.start
-.arm
-.global _start
-.type _start, %function
-
-_start:
-   //DO NOT EDIT THIS!
-   .word 0xE51FF004 
-   .word loop 
-   .word 0xE51FF004 
-   .word loop
-   .word 0xE51FF004 
-   .word loop
-   .word 0xE51FF004 
-   .word loop
-   .word 0xE51FF004 
-   .word loop
-   .word 0xE51FF004 
-   .word loop
-   //!!!	
-   
-	ldr sp, =0x24000000
-				
-    bl main
+@---------------------------------------------------------------------------------
+	.section ".init"
+@---------------------------------------------------------------------------------
+	.global _start
+	.extern main
+	.align	4
+	.arm
 	
-loop:
-    b loop
+_vectors:
+	ldr pc, =InfiniteLoop
+	.pool
+	ldr pc, =InfiniteLoop
+	.pool
+	ldr pc, =InfiniteLoop
+	.pool
+	ldr pc, =InfiniteLoop
+	.pool
+	ldr pc, =InfiniteLoop
+	.pool
+	ldr pc, =InfiniteLoop
+	.pool
+@---------------------------------------------------------------------------------
+_start:
+@---------------------------------------------------------------------------------
+	ldr sp,=0x22140000
+		
+		@@wait for the arm11 kernel threads to be ready
+		ldr r1, =0x10000
+		waitLoop9:
+			sub r1, #1
+
+			cmp r1, #0
+			bgt waitLoop9
+
+		ldr r1, =0x10000
+		waitLoop92:
+			sub r1, #1
+
+			cmp r1, #0
+			bgt waitLoop92
+			
+		ldr sp,=0x22160000
+		ldr	r3, =main
+		blx r3
+		
+.pool
+		
+InfiniteLoop:
+	b InfiniteLoop
