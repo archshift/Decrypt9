62c1d30e0bad7a58773c31f0e518da599adc2d1e
archshift <admin@archshift.com>
2015-02-01 16:38:18 -0800

    Added HID input for selecting operation, changed background to white

--- source/main.c
+++ source/main.c
@@ -5,15 +5,29 @@
 #include "common.h"
 #include "draw.h"
 #include "fs.h"
+#include "hid.h"
 #include "decryptor/padgen.h"
+
+void ClearTop()
+{
+    ClearScreen(TOP_SCREEN, RGB(255, 255, 255));
+    current_y = 0;
+}
 
 int main()
 {
-    ClearScreen(TOP_SCREEN, RGB(0, 255, 0));
-
+    ClearTop();
     InitFS();
 
-    Debug("Padgen: %s", ncchPadgen() == 0 ? "succeeded" : "failed");
+    Debug("A: NCCH Padgen");
+    while (true) {
+        u32 pad_state = InputWait();
+        if (pad_state & BUTTON_A) {
+            ClearTop();
+            Debug("Padgen: %s", ncchPadgen() == 0 ? "succeeded" : "failed");
+            break;
+        }
+    }
 
     DeinitFS();
     return 0;
