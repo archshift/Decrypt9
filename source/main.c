2bd5f22f3d5761e01a795837fa5109ebd769dfbb
archshift <admin@archshift.com>
2015-02-21 22:16:15 -0800

    Titlekey decryption has been tested to work in practice

--- source/main.c
+++ source/main.c
@@ -22,7 +22,7 @@ int main()
 
     Debug("A: NCCH Padgen");
     Debug("B: SD Padgen (untested)");
-    Debug("X: Titlekey Decryption (untested)");
+    Debug("X: Titlekey Decryption");
     Debug("Y: NAND Padgen (tested on 9.2)");
     while (true) {
         u32 pad_state = InputWait();
