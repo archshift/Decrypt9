c1bc425c27bf6e7de9dddc5c1c7536b34bca59bf
d0k3 <tore.anon@gmail.com>
2015-07-11 16:07:46 +0200

    Moved debug output to special file io functions
    o reason: this way the same checks and messages don't have to given in
    several places
    o this also enables safety checks where previously there where none
    o this commit also fixes the too long error message in NcchPadgen()
    o this commit also removes the redundant file name in NandDumper()
    o FileClose() is now handled outside of DebugFileRead() / DebugFileWrite()

--- source/fs.h
+++ source/fs.h
@@ -7,15 +7,19 @@ void DeinitFS();
 
 /** Opens existing files */
 bool FileOpen(const char* path);
+bool DebugFileOpen(const char* path);
 
 /** Opens new files (and creates them if they don't already exist) */
 bool FileCreate(const char* path, bool truncate);
+bool DebugFileCreate(const char* path, bool truncate);
 
 /** Reads contents of the opened file */
 size_t FileRead(void* buf, size_t size, size_t foffset);
+bool DebugFileRead(void* buf, size_t size, size_t foffset);
 
 /** Writes to the opened file */
 size_t FileWrite(void* buf, size_t size, size_t foffset);
+bool DebugFileWrite(void* buf, size_t size, size_t foffset);
 
 /** Gets the size of the opened file */
 size_t FileGetSize();
